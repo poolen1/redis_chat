859f912cf4df7438f66b179fcac42b1b18e5cec4
Nick <poolen108@gmail.com>
2022-01-21 17:31:44 -0800

    Redis connection, started main()

---
+++ redis_chat/redis_chat/ChatApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace redis_chat.ChatApp
+{
+    class Program
+    {
+        private const string RedisConnectionString = "172.16.36.94";
+        private static ConnectionMultiplexer connection =
+            ConnectionMultiplexer.Connect(RedisConnectionString);
+
+        private const string ChatChannel =
+            "RedisChat-Channel";
+        private static string userName = string.Empty;
+
+        static void Main()
+        {
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine();
+        }
+    }
+}
